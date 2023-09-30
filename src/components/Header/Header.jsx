818d88fa39a372dd458ba3df1a882bb2eb72a887
Alexandra1234 <alexandra.zakovinko>
2023-09-30 16:43:53 +0300

    add header block

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,10 @@ import './Header.css';
 
 export const Header = () => {
     return (
-        <header className="header"></header>
+        <header className="header">
+            <h1 className="header__title">Онлайн-подбор средств для лица</h1>
+            <p className="header__subtitle">Пройдите короткий тест и получите список наиболее подходящих для вас
+                косметических продуктов</p>
+        </header>
     )
 }
