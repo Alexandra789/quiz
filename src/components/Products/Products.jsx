a7a900db3f8742104cc21f882e21b8dc7bc4fc7c
Alexandra1234 <alexandra.zakovinko>
2023-09-30 21:27:39 +0300

    add product block

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,151 @@ import './Products.css';
 
 export const Products = () => {
     return (
-        <div className="products"></div>
+        <div className="products">
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+            <div className="card">
+                <div className="card__img">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg"
+                        alt="product image"/>
+                </div>
+                <h3 className="card__title">Бальзам от пигм.пятен для чув. кожи ISISPHARMA NEOTONE Sensitive 30мл</h3>
+                <div className="card__price">
+                    <p className="card__price__discount">115</p>
+                    <p className="card__price__current">98.65<span>руб</span></p>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
