c64494c244593ece429916af01f64cd79a6d1ad8
Alexandra1234 <alexandra.zakovinko>
2023-09-30 16:41:16 +0300

    formatted

--- src/components/QuestionsArea/QuestionsArea.jsx
+++ src/components/QuestionsArea/QuestionsArea.jsx
@@ -7,16 +7,16 @@ import {Counter} from "../Counter";
 import {Question} from "../Question";
 
 export const QuestionsArea = () => {
-    const [currentQuestion,setCurrentQuestion] = useState(1)
+    const [currentQuestion, setCurrentQuestion] = useState(1)
 
     return (
-       <div className="questions-area">
-           {currentQuestion <= questions.length ?
-            <div className="questions-area-wrapper">
-                <Counter currentQuestion={currentQuestion} questions={questions}/>
-                <Question currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}
-                          questions={questions}/>
-            </div> : '' }
-       </div>
+        <div className="questions-area">
+            {currentQuestion <= questions.length ?
+                <div className="questions-area-wrapper">
+                    <Counter currentQuestion={currentQuestion} questions={questions}/>
+                    <Question currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}
+                              questions={questions}/>
+                </div> : ''}
+        </div>
     )
 }
