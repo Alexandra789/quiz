509804dee5a7d6dd0ac8091a3d175b2adc07f2c6
Alexandra1234 <alexandra.zakovinko>
2023-09-30 21:28:05 +0300

    fix struct app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import {Header} from "./components/Header";
 import {QuestionsArea} from "./components/QuestionsArea";
 
 function App() {
     return (
-        <div className="container">
-            <Header/>
-            <QuestionsArea/>
-        </div>
+        <QuestionsArea/>
     );
 }
 
