3c3e249d69fabd5b9c8db27d7c11e89b9be22a1e
Alexandra1234 <alexandra.zakovinko>
2023-09-30 16:45:42 +0300

    add counter block

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,18 @@
 import './Counter.css';
 
-export const Counter = () => {
+export const Counter = (props) => {
+    const {currentQuestion, questions} = props;
     return (
-        <div className="counter"></div>
+        <div className="counter">
+            <div className="counter__pagination">
+                {
+                    questions.map(question => (
+                        <div key={question.id}
+                             className={question.id === currentQuestion ? "counter__pagination-bullet active" : "counter__pagination-bullet"}></div>
+                    ))
+                }
+            </div>
+            <p className="counter__text">Вопрос {currentQuestion} из {questions.length}</p>
+        </div>
     )
 }
